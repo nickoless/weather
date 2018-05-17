3a1a2cb567973d0b9615e4795aa9f7cfeebc3e7f
Nick Lee <nickoless.lee@gmail.com>
2018-05-17 21:11:47 +0200

    Adding design elements

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class Weather extends Component {
   constructor(props) {
     super (props);
-
     this.state = {
       city: null,
       temperature: null,
       weather: null,
-      icon: null,
       measurement: 'Celsius'
     };
-  }
+  };
  
   measurementHandler = () => {
     const imperial = Math.round(this.state.temperature * 1.8 + 32)
@@ -22,37 +21,60 @@ class Weather extends Component {
   };
 
   componentDidMount() {
+
     let apiKey = '643661576b7df360bb874c779f2389c8'
     let city = 'Berlin'
-    let url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&id=524901&APPID=${apiKey}`
-
-    fetch(url)
+    let weatherURL = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&id=524901&APPID=${apiKey}`
+    
+    fetch(weatherURL)
       .then((response) => response.json())
       .then((result) => {
         console.log(result)
         this.setState({
           weather: result.list[0].weather[0].main,
           temperature: Math.round(result.list[0].main.temp),
-          city: result.city.name
-        })
+          city: result.city.name,
+          country: result.city.country
+        });
       })
       .catch((error) => {
         console.error(error);
       })
+  };
+
+  render() {
     
-  }
-
-  render() {    
-
-  
-
+    let icon = null;
+    let weather = this.state.weather
+    
+    if (weather === 'Clouds') { 
+      icon = 'wi wi-cloud'
+    }
+    if (weather === 'Clear') {
+      icon = 'wi wi-day-sunny'
+    } 
+    if (weather === 'Rain') {
+      icon = 'wi wi-rain' 
+    }
+      
     return(
-      <div>
-        <h1>{this.state.city}</h1>
-        <p>{this.state.weather}<i className="wi wi-night-sleet"></i></p>
-        <p>{this.state.temperature}</p>
-        <p>{this.state.measurement}</p>
-        <button onClick={this.measurementHandler}>click me!</button>
+      <div className='container'>
+        <p className='city'>{this.state.city} <span className='country'>{this.state.country}</span></p>
+        <i className={icon}></i>
+        <div className='weather'>{this.state.weather}</div><hr/>
+        <div class="row">
+
+        <div class="col-xs-6">
+          {this.state.temperature}° {this.state.measurement}
+          <button type='button' className='btn btn-outline-primary' onClick={this.measurementHandler}>UNITS</button>
+
+        </div>
+        <div class="col-xs-6">
+          Other shit
+        </div>
+
+</div>
+
       </div>
     )
   };
